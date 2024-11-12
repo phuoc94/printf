f69500b21074073669e407cb7f96f4368aa234f6
Phuoc Nguyen <phuoc94@hotmail.fi>
2024-11-12 22:57:24 +0200

    done print character and string

--- ft_printf.c
+++ ft_printf.c
@@ -6,92 +6,56 @@
 /*   By: phuocngu <phuocngu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 14:59:02 by phuocngu          #+#    #+#             */
-/*   Updated: 2024/11/12 15:06:39 by phuocngu         ###   ########.fr       */
+/*   Updated: 2024/11/12 22:55:34 by phuocngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
 #include <stdio.h>
-#include <stdarg.h>
-#include <unistd.h>
 
-void ft_putchar(char c)
+int	print_char(char c)
 {
-	write(1, &c, 1);
+	return (write(1, &c, 1));
 }
 
-void ft_putstr(char *s)
+int	print_str(char *s)
 {
-	int i;
+	int	i;
 
+	if (!s)
+		s = "(null)";
 	i = 0;
-	while (s[i])
+	while (s[i] != '\0')
 	{
-		ft_putchar(s[i]);
+		write(1, &s[i], 1);
 		i++;
 	}
+	return (i);
 }
 
-void	ft_putnbr(int nb)
+int	ft_printf(const char *str, ...)
 {
-	int	div;
-	int	mod;
-
-	div = nb / 10;
-	mod = nb % 10;
-	if (nb == -2147483648)
-	{
-		ft_putchar('-');
-		ft_putchar('2');
-		ft_putnbr(147483648);
-		return ;
-	}
-	if (nb < 0)
-	{
-		ft_putchar('-');
-		nb = -nb;
-		ft_putnbr(nb);
-		return ;
-	}
-	if (nb >= 10)
-	{
-		ft_putnbr(div);
-	}
-	ft_putchar('0' + mod);
-}
-
-int ft_printf(const char *str, ...)
-{
-
 	int i;
-	va_list	args;
+	va_list args;
+	int result_len;
+
 	i = 0;
-
-
+	result_len = 0;
 	va_start(args, str);
-	while (str[i])
+	while (str[i] != '\0')
 	{
-		if (str[i] == '%')
+		if (str[i] == '%' && str[i + 1] != '\0')
 		{
 			i++;
-			if(str[i] == 's')
-				ft_putstr(va_arg(args, char *));
-			else if(str[i] == 'c')
-				ft_putchar(va_arg(args, int));
-			else if(str[i] == 'd' || str[i] == 'i')
-				ft_putnbr(va_arg(args, int));
-
-		}else
-			ft_putchar(str[i]);
+			if (str[i] == 'c')
+				result_len += print_char(va_arg(args, int));
+			else if (str[i] == 's')
+				result_len += print_str(va_arg(args, char *));
+		}
+		else
+			result_len += print_char(str[i]);
 		i++;
 	}
 	va_end(args);
-
-	return (0);
-}
-int main()
-{
-    int result = ft_printf("H%s, worl%c.\n", "ello", 'd');
-
-	(void)result;
-	return (0);
+	return (result_len);
 }
---
+++ ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phuocngu <phuocngu@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 22:12:16 by phuocngu          #+#    #+#             */
+/*   Updated: 2024/11/12 22:14:14 by phuocngu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_printf(const char *str, ...);
+
+#endif
